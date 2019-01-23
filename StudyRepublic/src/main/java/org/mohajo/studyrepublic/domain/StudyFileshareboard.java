ddff584fa843a561883c23ae6d8bf5999f73d906
exisign <exisign777@gmail.com>
2019-01-23 14:03:39 +0900

    ENH : domain / repository / studypage : domain, repository, studypage 클래스 추가

---
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/domain/StudyFileshareboard.java
@@ -0,0 +1,33 @@
+package org.mohajo.studyrepublic.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author 신상용
+ * @since 2019.01.22
+ * @version
+ * StudyFileshareboard domain 클래스 추가
+ */
+
+@Getter
+@Setter
+@Entity
+@Table(name = "study_fileshareboard")
+public class StudyFileshareboard extends StudyBoard{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "study_fileshareboard_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int studyFileshareboardId;
+}
---
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/repository/StudyFileshareboardReplyRepository.java
@@ -0,0 +1,8 @@
+package org.mohajo.studyrepublic.repository;
+
+import org.mohajo.studyrepublic.domain.StudyFileshareboardReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyFileshareboardReplyRepository extends JpaRepository<StudyFileshareboardReply, Integer>{
+
+}
