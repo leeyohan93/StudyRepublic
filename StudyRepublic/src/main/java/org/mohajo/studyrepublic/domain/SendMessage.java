ea403ad619857482362df0055c0e5d3b0f35614f
kimjs <kjs921024@naver.com>
2019-01-25 10:46:24 +0900

    ENH : /domain/templates : ReceiveMessage,SendMessage에 어노테이션추가 mypage_main HTML 추가

--- StudyRepublic/src/main/java/org/mohajo/studyrepublic/domain/SendMessage.java
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/domain/SendMessage.java
@@ -12,6 +12,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -27,6 +29,7 @@ public class SendMessage extends Message implements Serializable{
 	private static final long serialVersionUID= 1L;
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="message_send_id" , nullable=false)
 	private int messageSendId;
 	
