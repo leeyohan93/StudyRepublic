44428060b71ab160b0edc4c1c089cff1ccbf5cc2
yohan <good_1411@naver.com>
2019-02-07 14:15:30 +0900

    ENH: Admin: layout 추가,Template 적용 및 수정
    - layout추가하였고 그 기반으로 admin member 진행중

--- StudyRepublic/src/main/java/org/mohajo/studyrepublic/admin/AdminMemberController.java
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/admin/AdminMemberController.java
@@ -1,4 +1,13 @@
 package org.mohajo.studyrepublic.admin;
+
+import java.util.List;
+
+import org.mohajo.studyrepublic.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author 이요한
  * @since 2019.01.22
@@ -6,7 +15,21 @@ package org.mohajo.studyrepublic.admin;
  * -기능1 추가
  * -기능2 추가
  * -기능3 추가
+ * 
  */
+@Controller
+@RequestMapping("/adminPage/member")
 public class AdminMemberController {
+	
+	@Autowired
+	AdminMemberService adminMemberService;
+	
+	@RequestMapping("/list")
+	public String list(Model model) {
+		
+		model.addAttribute("memberList", adminMemberService.getMemberList());
+
+		return "/adminPage/member/list";
+	}
 
 }
---
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/admin/AdminMemberService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.mohajo.studyrepublic.admin;
+
+import java.util.List;
+
+import org.mohajo.studyrepublic.domain.Member;
+
+/**
+ * @author 이요한
+ * @since 2019. 2. 7.
+ * @version 0.0
+ * -기능 설명1
+ */
+public interface AdminMemberService {
+
+	List<Member> getMemberList();
+
+}
