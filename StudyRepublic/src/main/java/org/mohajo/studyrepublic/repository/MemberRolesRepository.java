12b6f790fba6808405aefa8926954a82e4803c07
seonghochoigo <dsds550@gmail.com>
2019-02-14 09:26:39 +0900

    ENH:VIEW:강사신청폼
    강사신청폼 완성

--- StudyRepublic/src/main/java/org/mohajo/studyrepublic/repository/MemberRolesRepository.java
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/repository/MemberRolesRepository.java
@@ -4,10 +4,13 @@ package org.mohajo.studyrepublic.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.mohajo.studyrepublic.domain.MemberRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
+
 
 /**
  * @author 윤성호
@@ -17,13 +20,15 @@ import org.springframework.transaction.annotation.Transactional;
  * 
  */
 
-@Transactional(readOnly = true)
+
 public interface MemberRolesRepository extends JpaRepository<MemberRoles, String>{
 	
 		@Query(value = "select * from member_roles where member = :id", nativeQuery = true)
 		List<MemberRoles> findByRole(String id);
 		
-		
+
+		@Modifying
+		@Transactional
 		@Query(value = "delete from member_roles where member = :id && role_name = 'W'", nativeQuery = true)
-		MemberRoles deleteTutorWait(String id);
+		void deleteTutorWait(String id);
 }
