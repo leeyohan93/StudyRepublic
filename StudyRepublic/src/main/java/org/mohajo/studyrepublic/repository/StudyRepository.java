b57d3e93c3af03854a28a5f51bdf53aeaf036995
yohan <good_1411@naver.com>
2019-02-25 14:31:58 +0900

    ENH: main: 일반인기스터디 로직수정, 데이터 폼 추가
    - 쿼리문 수정
    - 인기 스터디 없을 시 나오는 데이터 폼 추가

--- StudyRepublic/src/main/java/org/mohajo/studyrepublic/repository/StudyRepository.java
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/repository/StudyRepository.java
@@ -63,8 +63,7 @@ public interface StudyRepository extends JpaRepository<Study, String>, QuerydslP
 //	public List<Study> findPrStudyBytypeCode();
 	public List<PopularStudy> findPrStudyBytypeCode();
 	
-	@Query(value="select * from study natural join study_interest where interest_2_code in (?1) "
-			+ "and STUDY_STATUS_CODE ='O' order by rand() limit 2",nativeQuery=true)
+	@Query(value="select * from study where study_id in (select distinct study_id from study natural join study_interest where interest_2_code in (?1) and TYPE_CODE = 'B' and STUDY_STATUS_CODE ='O') order by rand() limit 2;",nativeQuery=true)
 //	public List<Study> findBsStudyBytypeCode();
 	public List<PopularStudy> findBsStudyBytypeCode(String[] popularTag);
 	
