b97dbef67d8db090bc237f2e2727968f1132a810
miyeunlee <miyeun.mia.lee@gmail.com>
2019-02-27 09:45:36 +0900

    ADD img sources

---
+++ StudyRepublic/src/main/java/org/mohajo/studyrepublic/repository/StudyPriceRepository.java
@@ -0,0 +1,17 @@
+package org.mohajo.studyrepublic.repository;
+
+import org.mohajo.studyrepublic.domain.Study;
+import org.mohajo.studyrepublic.domain.StudyPrice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+/**
+ * @author	이미연
+ * @since	2019. 2. 26.
+ * @version	0.0
+ * - 기능 설명 1
+ */
+public interface StudyPriceRepository extends JpaRepository<StudyPrice, String>, QuerydslPredicateExecutor<Study> {
+
+
+}
